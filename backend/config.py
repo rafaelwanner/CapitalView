26fdfb069ba86c0f5e89ef7e29267b8ea18cf373
rwanner <wanner.rafael@icloud.com>
2020-08-26 21:30:17 +0200

    [SECURITY] database credentials removed

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ import os
 
 class Config(object):
     SECRET_KEY = 'TEST'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://rwan:Rafael1997@localhost/capitalview'#os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = 5*60 #5min
