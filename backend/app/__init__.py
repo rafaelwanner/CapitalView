e61441a5f0ace3ec749b7542cb021158056df46f
rwanner <wanner.rafael@icloud.com>
2020-08-27 16:05:11 +0200

    cross origin request ability added

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from config import Config
 
 app = Flask(__name__)
@@ -10,5 +11,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
+CORS(app)
 
 from app import endpoints, models
